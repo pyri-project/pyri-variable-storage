7919cee3bf3569abaada1db9950a7530c7c79b9e
John Wason <wason@wasontech.com>
2021-08-10 00:20:11 -0400

    Add --version option to service(s)

--- src/pyri/variable_storage/__main__.py
+++ src/pyri/variable_storage/__main__.py
@@ -8,6 +8,7 @@ from RobotRaconteurCompanion.Util.InfoFileLoader import InfoFileLoader
 from RobotRaconteurCompanion.Util.AttributesUtil import AttributesUtil
 from pyri.plugins import robdef as robdef_plugins
 from pyri.util.service_setup import PyriServiceNodeSetup, PyriService_NodeSetup_Default_Flags
+import importlib.metadata
 
 PyriService_VariableStorage_NodeSetup_Default_Flags = PyriService_NodeSetup_Default_Flags | RR.RobotRaconteurNodeSetupFlags_JUMBO_MESSAGE
 
@@ -16,6 +17,10 @@ def main():
     parser_group = parser.add_mutually_exclusive_group(required=True)
     parser_group.add_argument("--db-file", type=str,default=None,help="Variable database filename")
     parser_group.add_argument("--db-url", type=str,default=None,help="SQLAlchemy connections string (use --db-file for SQLite)")
+
+    # Add version here so it doesn't raise an error...
+    version1 = importlib.metadata.version('pyri-variable-storage')
+    parser.add_argument('-V', '--version', action='version', version=f'%(prog)s {version1}')
     args, _ = parser.parse_known_args()
         
     if args.db_file:
@@ -27,7 +32,8 @@ def main():
 
     with PyriServiceNodeSetup("tech.pyri.variable_storage",59901, flags=PyriService_VariableStorage_NodeSetup_Default_Flags, \
         default_info = (__package__,"pyri_variable_storage_default_info.yml"), \
-        arg_parser = parser, no_device_manager=True, register_plugin_robdef=True) as service_node_setup:
+        arg_parser = parser, no_device_manager=True, register_plugin_robdef=True, \
+        ) as service_node_setup:
 
         extra_imports = RRN.GetRegisteredServiceTypes()
         db.device_info=service_node_setup.device_info_struct
